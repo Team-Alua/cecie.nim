e33851795a78b352c05829ead7ebe361ea14f18e
ac2pic <ac2pic@gmail.com>
2023-04-02 03:22:33 -0500

    Major improvements

--- test.nim
+++ test.nim
@@ -1,79 +1,70 @@
 import bitops
 import strutils
-import "orbis/SaveData"
-import "orbis/_types/save_data"
-import "orbis/UserService"
-import "orbis/_types/user"
-import "orbis/_types/errors"
+import "orbis/savedata"
+import "orbis/errors" 
+import "orbis/userservice"
+import "orbis/procinfo"
+import "orbis/path"
+import "libjbc"
+
 import os
 import asyncnet
 import asyncdispatch
 
-
-
 proc inf = 
   while true:
     discard
-echo "Hi!"
-var initParams : OrbisUserServiceInitializeParams
-initParams.priority = 700
-if ORBIS_OK != sceUserServiceInitialize(addr(initParams)):
+echo "Hi"
+if ORBIS_OK != userservice.init():
   echo "Failed to initialize user service"
   inf()
 
-proc doMount(client: AsyncSocket) {.async.} = 
+if ORBIS_OK != savedata.init():
+  echo "Failed to initialize sceSaveData"
+  inf()
 
-  if ORBIS_OK != sceSaveDataInitialize3(0):
-    await client.send("\nFailed to initialize sceSaveData")
-    return  
+proc doMount(client: AsyncSocket) {.async.} = 
   var userId: int32
-  
-  if ORBIS_OK != sceUserServiceGetInitialUser(addr(userId)):
+  if ORBIS_OK != getUserId(userId):
     await client.send("\nFailed to get initial user")
     return
- 
   await client.send("\nMounting...")
   await client.send("\nUserId: " & $userId)
-  var mount : OrbisSaveDataMount
-  mount.userId = userId
-  
-  mount.dirName = alignLeft("2", 32, '\x00').cstring
-  mount.titleId = alignLeft("DEVP00001", 16, '\x00').cstring
 
-  const fingerprint = repeat('0', 64) & repeat('\x00', 80 - 64)
-  mount.fingerprint = fingerprint.cstring
-  mount.blocks = 114.uint64
-  mount.mountMode = bitor(ORBIS_SAVE_DATA_MOUNT_MODE_DESTRUCT_OFF,
-                       ORBIS_SAVE_DATA_MOUNT_MODE_CREATE2,
-                       ORBIS_SAVE_DATA_MOUNT_MODE_RDWR)
-  
-  var mountResult: OrbisSaveDataMountResult
-  var errCode = sceSaveDataMount(addr(mount), addr(mountResult))
-  
-  if ORBIS_OK != errCode:
-    await client.send("\nsceSaveDataMount() = " & errCode.toHex(8))
+  var saveData: SaveData  
+  saveData.userId = userId
+  saveData.titleId = APP_TITLE_ID
+  saveData.dirName = "2"
+  const fingerprint = repeat('0', 64)
+  saveData.fingerprint = fingerprint
+  saveData.blocks = 114
+  saveData.mountMode(SaveDataMountMode.DESTRUCT_OFF,
+                     SaveDataMountMode.CREATE2,
+                     SaveDataMountMode.RDWR) 
+
+  await client.send("\n" & $saveData)
+  var mounter : SaveDataMounter
+
+  mounter.data = saveData
+  let mr: SaveDataMounterResult = mounter.mount()
+
+  if ORBIS_OK != mr.code:
+    await client.send("\nsceSaveDataMount() = " & mr.code.toHex(8))
     return
   else:
     await client.send("\nMounted successfully")
   
-  var mp : OrbisSaveDataMountPoint
-  await client.send("\nMount path:")
-  var pathName: string
+  await client.send("\nMount path:" & mr.mountPath)
 
-  for i in mountResult.mountPathName:
-    if i == '\x00':
-      break
-    pathName.add(i)
-  pathName = joinPath(pathName, "")
-  for path in walkDirRec(pathName, checkDir=true, skipSpecial=true):
-    await client.send("\n" & path)
+  sudo:
+    for path in walkDirRec(fromSandboxToAbsPath(mr.mountPath)):
+      echo path
 
-  mp.data = mountResult.mountPathName
   
-  let result2 = sceSaveDataUmount(addr(mp))
+  var umr = mounter.unmount()
   
-  if ORBIS_OK != result2:
-    await client.send("\nFailed to unmount" & result2.toHex(8))
+  if ORBIS_OK != umr:
+    await client.send("\nFailed to unmount" & umr.toHex(8))
   else:
     await client.send("\nunmount success")
 
@@ -85,7 +76,6 @@ proc main {.async.} =
 
   while true:
     let client = await server.accept()
-
     await client.send("Doing mount")
     await doMount(client) 
     client.close()
